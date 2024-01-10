3379841ecec2a7e70eede28e11a69d8b82a6274e
Shada Al-Wakkal <74023297+Shada12354@users.noreply.github.com>
2024-01-10 17:16:40 +0100

    Create HelloWorld.java
    Hello World!

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import static java.lang.System.*;
+
+public class HelloWorld{
+    public static void main(String [] args){
+        new HelloWorld().program();
+    }
+    void program(){
+        out.println("Hello World!!");
+    }
+}
